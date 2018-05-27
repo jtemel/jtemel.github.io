1f71131c29a3202810d2bfd1253ff1e8bb5b093f
Jonathon Temelkovski <jonathon.temelkovski@mail.utoronto.ca>
2018-05-26 21:30:14 -0400

    Initial commit.
    Working version of the website

---
+++ js/name.js
@@ -0,0 +1,13 @@
+// This is used to type the title,
+// mimicing a common spelling along the way.
+$(function(){
+    $(".name").typed({
+        strings: [
+            "onathan",
+            "onathon Temelkovski"
+        ],
+        typeSpeed: 30,
+        backSpeed: 40,
+        backDelay: 300
+    });
+});
